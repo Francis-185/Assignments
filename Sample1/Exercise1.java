7f38519db06ea5d57a7076cec5c4414d9805877d
Francis-185 <168449010+Francis-185@users.noreply.github.com>
2024-04-29 09:18:14 -0500

    Add files via upload

---
+++ Sample1/Exercise1.java
@@ -0,0 +1,31 @@
+package com.Sample1;
+
+public class Exercise1 {
+
+	public static void main(String[] args) {
+		int x=10,y=10;
+		int z = x+y;
+		int[] myNum = {10, 20, 30, 40};
+		System.out.println(z);
+		String make ="TOYOTA";
+		System.out.println(make);
+		System.out.println(make.length());
+		System.out.println(make.toLowerCase());
+		String[] cars ={"bmw","[ford]","[audi]","[toyota"};
+		System.out.println(cars[0]);
+		System.out.println(myNum.length);
+
+String firstName = "John ";
+String lastName = "Doe";
+System.out.println(firstName.concat(lastName));
+
+String[] carS= {"Volvo", "BMW", "Ford"};
+for
+ (String i : carS) {
+  System.out.println(i);
+}
+		
+		
+	}
+
+}
---
+++ Sample1/Francis.java
@@ -0,0 +1,11 @@
+package com.Sample1;
+
+public class Francis {
+
+	public static void main(String[] args) 
+	{
+		System.out.println("Hello Java");
+
+	}
+
+}
